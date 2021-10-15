c42daf815431e32f98f2f6c1da6c15d9bd9d9a46
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-10-15 11:55:30 -0300

    V1

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,12 @@
 FROM python:3 AS builder
-ADD . /app
-WORKDIR /app
-
-# We are installing dependencies here directly into our app source dir
+RUN mkdir /app
 RUN pip install --target=/app oca-maintainers-tools@git+https://github.com/OCA/maintainer-tools.git
 
 # A distroless container image with Python and some basics like SSL certificates
 # https://github.com/GoogleContainerTools/distroless
 FROM gcr.io/distroless/python3-debian10
 COPY --from=builder /app /app
+COPY . /app/
 WORKDIR /app
 ENV PYTHONPATH /app
 CMD ["/app/main.py"]
--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def get_bool(input):
 
 
 if get_bool("gen-addons-readme"):
-    bin_path = "/app/bin/oca-gen-addon-readme"
+    cmd = ["python", "-m", "tools.gen_addon_readme"]
     args = [
         "--addons-dir", get("addons-dir"),
         "--org-name", get("organization"),
@@ -35,25 +35,26 @@ if get_bool("gen-addons-readme"):
         "--commit" if get_bool("commit") else "--no-commit",
         "--gen-html" if get_bool("gen-html") else "--no-gen-html",
     ]
-    check_call([bin_path] + args)
+    check_call(cmd + args)
 
 
 if get_bool("gen-addons-icon"):
-    bin_path = "/app/bin/oca-gen-addon-icon"
+    cmd = ["python", "-m", "tools.gen_addon_icon"]
     args = [
         "--addons-dir", get("addons-dir"),
         "--commit" if get_bool("commit") else "--no-commit",
     ]
     if get("src-path"):
         args += ["--src-path", get("src-path")]
-    check_call([bin_path] + args)
+    check_call(cmd + args)
 
 
 if get_bool("gen-addons-table"):
-    bin_path = "/app/bin/oca-gen-addons-table"
+    bin_path = "/venv/bin/oca-gen-addons-table"
+    cmd = ["python", "-m", "tools.gen_addons_table"]
     args = [
         "--addons-dir", get("addons-dir"),
         "--readme-path", get("readme-path"),
         "--commit" if get_bool("commit") else "--no-commit",
     ]
-    check_call([bin_path] + args)
+    check_call(cmd + args)
